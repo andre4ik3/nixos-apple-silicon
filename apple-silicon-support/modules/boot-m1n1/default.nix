9503712656cb7e5ba17f673486ae398c64f34278
andre4ik3 <andre4ik3@fastmail.com>
2024-10-20 23:28:40 +0400

    m1n1 and U-boot package overrides

--- apple-silicon-support/modules/boot-m1n1/default.nix
+++ apple-silicon-support/modules/boot-m1n1/default.nix
@@ -1,24 +1,12 @@
 { config, pkgs, lib, ... }:
 let
-  pkgs' = config.hardware.asahi.pkgs;
-
-  bootM1n1 = pkgs'.m1n1.override {
-    isRelease = true;
-    withTools = false;
-    customLogo = config.boot.m1n1CustomLogo;
-  };
-
-  bootUBoot = pkgs'.uboot-asahi.override {
-    m1n1 = bootM1n1;
-  };
-
   bootFiles = {
     "m1n1/boot.bin" = pkgs.runCommand "boot.bin" {} ''
-      cat ${bootM1n1}/build/m1n1.bin > $out
+      cat ${config.boot.loader.asahi.m1n1.package}/build/m1n1.bin > $out
       cat ${config.boot.kernelPackages.kernel}/dtbs/apple/*.dtb >> $out
-      cat ${bootUBoot}/u-boot-nodtb.bin.gz >> $out
-      if [ -n "${config.boot.m1n1ExtraOptions}" ]; then
-        echo '${config.boot.m1n1ExtraOptions}' >> $out
+      cat ${config.boot.loader.asahi.uboot.package}/u-boot-nodtb.bin.gz >> $out
+      if [ -n "${config.boot.loader.asahi.m1n1.extraOptions}" ]; then
+        echo '${config.boot.loader.asahi.m1n1.extraOptions}' >> $out
       fi
     '';
   };
@@ -33,23 +21,46 @@ in {
     system.build.m1n1 = bootFiles."m1n1/boot.bin";
   };
 
-  options.boot = {
-    m1n1ExtraOptions = lib.mkOption {
-      type = lib.types.str;
-      default = "";
-      description = ''
-        Append extra options to the m1n1 boot binary. Might be useful for fixing
-        display problems on Mac minis.
-        https://github.com/AsahiLinux/m1n1/issues/159
-      '';
+  imports = [
+    (lib.mkRenamedOptionModule [ "boot" "m1n1ExtraOptions" ] [ "boot" "loader" "asahi" "m1n1" "extraOptions" ])
+    (lib.mkRenamedOptionModule [ "boot" "m1n1CustomLogo" ] [ "boot" "loader" "asahi" "m1n1" "customLogo" ])
+  ];
+
+  options.boot.loader.asahi = {
+    m1n1 = {
+      package = lib.mkPackageOption pkgs "Custom package override for Asahi m1n1" {
+        default = pkgs.m1n1.override {
+          isRelease = true;
+          withTools = false;
+          customLogo = config.boot.loader.asahi.m1n1.customLogo;
+        };
+      };
+
+      extraOptions = lib.mkOption {
+        type = lib.types.str;
+        default = "";
+        description = ''
+          Append extra options to the m1n1 boot binary. Might be useful for fixing
+          display problems on Mac minis.
+          https://github.com/AsahiLinux/m1n1/issues/159
+        '';
+      };
+
+      customLogo = lib.mkOption {
+        type = lib.types.nullOr lib.types.path;
+        default = null;
+        description = ''
+          Custom logo to build into m1n1. The path must point to a 256x256 PNG.
+        '';
+      };
     };
 
-    m1n1CustomLogo = lib.mkOption {
-      type = lib.types.nullOr lib.types.path;
-      default = null;
-      description = ''
-        Custom logo to build into m1n1. The path must point to a 256x256 PNG.
-      '';
+    uboot = {
+      package = lib.mkPackageOption pkgs "Custom package override for Asahi U-Boot" {
+        default = pkgs.uboot-asahi.override {
+          m1n1 = config.boot.loader.asahi.m1n1.package;
+        };
+      };
     };
   };
 }
